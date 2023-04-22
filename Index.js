0c9cc4130046e57e52a316b89b8e1b083ba98049
julio-cesar <112985882+Julio-73@users.noreply.github.com>
2023-04-22 17:34:26 -0500

    ------> new commit ---->

---
+++ Index.js
@@ -0,0 +1,21 @@
+
+
+// h1
+    ScrollReveal({
+         reset: true,
+         distance: '100px',
+         duration:900,
+         delay:400
+         });
+
+         ScrollReveal().reveal('.cal', { origin: 'right' });
+
+// form
+         ScrollReveal({
+            reset: true,
+            distance: '20px',
+            duration:500,
+            delay:200
+            });
+
+            ScrollReveal().reveal('form', { origin: 'bottom' });
